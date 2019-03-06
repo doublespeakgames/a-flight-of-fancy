bffdd0fba60ca0ead19cf518fced8d2550312ed7
Michael Townsend <michael@doublespeakgames.com>
2019-03-06 12:56:15 -0500

    Fixed the skip-ahead feature

--- app/src/action/restart.mjs
+++ app/src/action/restart.mjs
@@ -4,30 +4,25 @@ import type { ActionHandler, ActionResult } from '../action-resolver';
 import Config from '../config';
 import { deleteSession } from '../store';
 
+
 const restart:ActionHandler = (session, world, subject) => {
 
   if (!Config.dev || !subject) {
-    // TODO: This needs confirmation
-    deleteSession(session._id);
-    return {
-      message: `Okay, we'll start over.`,
-      close: true
-    };
+    subject = 'pantry';
   }
 
-  if (subject === 'tent') {
-    return {
-      message: 'Skipping ahead to the tent.',
-      update: {
-        room: 'tent',
-        inventory: new Set(),
-        flags: {},
-        effects: new Set(),
-        seen: new Set(),
-        gone: new Set()
-      }
-    };
-  }
+  // TODO: This needs confirmation
+  return {
+    message: `Starting at the ${subject}`,
+    update: {
+      room: subject,
+      inventory: new Set(),
+      flags: {},
+      effects: new Set(),
+      seen: new Set(),
+      gone: new Set()
+    }
+  };
 
 };
 
