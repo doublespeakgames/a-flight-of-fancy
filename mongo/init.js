48fb4829374d85e45655f88c7a8dda01f4ed1208
Michael Townsend <michael@doublespeakgames.com>
2019-03-26 10:38:23 -0400

    extend session lifespan to a week

--- mongo/init.js
+++ mongo/init.js
@@ -1,2 +1,2 @@
-db.session.createIndex( { "updated": 1 }, { expireAfterSeconds: 86400 } );
+db.session.createIndex( { "updated": 1 }, { expireAfterSeconds: 604800 } );
 db.canary.insert({ when: Date.now(), value: 'It worked?' });
